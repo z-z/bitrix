bc97b814f1131b77d2f5ec4b0ac11efbc446ea33
z-z <bazzaurik@gmail.com>
2021-06-27 14:33:11 +0300

    refactor

--- index.php
+++ index.php
@@ -10,9 +10,19 @@ $app     = Application::getInstance();
 $context = $app->getContext();
 $request = $context->getRequest();
 
-[$controller, $actionName] = Resolver::getControllerAndAction($_GET['vendor'], $_GET['module'], "{$_GET['scope']}.{$_GET['action']}");
+[$controller, $actionName] = Resolver::getControllerAndAction(
+    $_GET['vendor'],
+    $_GET['module'],
+    "{$_GET['scope']}.{$_GET['action']}"
+);
 
 if(!$controller || !method_exists($controller, $controller->generateActionMethodName($actionName)))
-	[$controller, $actionName] = Resolver::getControllerAndAction($_GET['vendor'], $_GET['module'], "{$_GET['scope']}.index.notfound");
+{
+    [$controller, $actionName] = Resolver::getControllerAndAction(
+        $_GET['vendor'],
+        $_GET['module'],
+        "{$_GET['scope']}.index.notfound"
+    );
+}
 
 $app->runController($controller, $actionName);
